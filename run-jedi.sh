c2ded1a167180af466d2a90d346f44bbc9623a4f
Andres Sewell <sewellandres@gmail.com>
2025-03-27 17:08:47 +0100

    Make run script scale with more nodes

--- run-jedi.sh
+++ run-jedi.sh
@@ -7,4 +7,4 @@ module load CMake
 module load CUDA
 
 export PENNINGTRAP_BINDIR=/p/project1/jureap1/sewell1/ippl/build/alpine
-srun --ntasks=1 --cpu-bind=cores --export=ALL,OMP_PROC_BIND=spread,OMP_PLACES=threads $PENNINGTRAP_BINDIR/PenningTrap 16 16 16 4096 20 FFT 0.01 LeapFrog --info 5
+srun --ntasks-per-node=1 --cpu-bind=cores --export=ALL,OMP_PROC_BIND=spread,OMP_PLACES=threads $PENNINGTRAP_BINDIR/PenningTrap 16 16 16 4096 20 FFT 0.01 LeapFrog --info 5
